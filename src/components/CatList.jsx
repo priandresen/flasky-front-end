5afce36dcdbf7f114281aad275c7d0830bee3c3d
Priscilla Andresen <pgdamorim@gmail.com>
2025-12-12 11:18:51 -0800

    main

---
+++ src/components/CatList.jsx
@@ -0,0 +1,35 @@
+import Cat from './Cat';
+import PropTypes from 'prop-types';
+
+const CatList = ({ cats }) => {
+	const catComponents = cats.map((cat, i) => {
+		return (
+			<Cat
+			key={i}
+			name={cat.name} 
+			personality={cat.personality} 
+			caretaker={cat.caretaker}
+			color={cat.color}
+			/>
+		);
+});
+		return (
+			<>
+        <h1>Cat List</h1>
+        <ul>
+					{catComponents}
+				</ul>
+    </>
+    );
+};
+
+CatList.propTypes = {}
+	cats: PropTypes.arrayOf(PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		personality: PropTypes.string.isRequired,
+		color: PropTypes.string.isRequired,
+		caretaker: PropTypes.string.isRequired
+	})).isRequired
+};
+
+export default CatList;
